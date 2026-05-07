8dba161d60de3b128b5a1b8164ad9fafa29c7af1
Vítor Fernandes Prochnow <vitor.prochnow@unimedlitoral.coop.br>
2026-05-07 11:47:18 -0300

    deixando logger para apenas mostrar INFO

--- utils/logger.py
+++ utils/logger.py
@@ -1,7 +1,7 @@
 import logging
 
 logging.basicConfig(
-    level=logging.DEBUG,
+    level=logging.INFO,
     filename='app.log',
     filemode='w',  
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
