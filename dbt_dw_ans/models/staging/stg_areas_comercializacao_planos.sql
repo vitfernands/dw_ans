3bdd90276c1ee50bbd6b59090487e75b6a3b2eb4
vitfernands <vitorfernandes0763@gmail.com>
2026-05-28 23:27:32 -0300

    alteracoes no dbt, deixando o materializaed: view e criando nova view

---
+++ dbt_dw_ans/models/staging/stg_areas_comercializacao_planos.sql
@@ -0,0 +1,13 @@
+SELECT
+    TRIM(id_plano)::VARCHAR(20)     AS id_plano,
+    TRIM(cd_plano)::VARCHAR(20)     AS cd_plano,
+    TRIM(cd_operadora)::INT         AS cd_operadora,
+    TRIM(cd_nota)::INT              AS cd_nota,
+    TRIM(dt_ntrp)::TIMESTAMP        AS dt_ntrp,
+    TRIM(cd_municipio)::INT         AS cd_municipio,
+    nm_municipio::VARCHAR(255)      AS nm_municipio,
+    TRIM(sg_uf)::VARCHAR(2)         AS sg_uf,
+    nm_regiao::VARCHAR(255)         AS nm_regiao,
+    TRIM(dt_atualizacao)::TIMESTAMP AS dt_atualizacao
+
+FROM {{source('stage', 'areas_comercializacao_planos') }}
