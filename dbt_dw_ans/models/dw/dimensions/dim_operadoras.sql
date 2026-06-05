9ebd6a885bd15434cacebed785a2d866a485f7e0
vitfernands <vitorfernandes0763@gmail.com>
2026-06-05 10:21:10 -0300

    configurando dbt para implementar as tabelas no dw e criando primeira tabela dimensao

---
+++ dbt_dw_ans/models/dw/dimensions/dim_operadoras.sql
@@ -0,0 +1,20 @@
+WITH operadoras AS (
+    SELECT * FROM {{ ref('stg_operadoras_ativas') }}
+),
+
+caracteristicas AS (
+    SELECT DISTINCT
+        registro_operadora,
+        porte_operadora
+    FROM {{ ref('stg_caracteristicas_produtos_suplementares') }}
+)
+
+SELECT DISTINCT
+    {{ dbt_utils.generate_surrogate_key(['o.registro_ans']) }} AS id_operadora,
+    o.registro_ans,
+    o.razao_social,
+    o.nome_fantasia,
+    o.modalidade,
+    c.porte_operadora
+FROM operadoras o
+LEFT JOIN caracteristicas c ON o.registro_ans::VARCHAR = c.registro_operadora
