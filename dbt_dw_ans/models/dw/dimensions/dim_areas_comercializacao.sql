a3433d5936de6f7936316db76c56055e713024db
vitfernands <vitorfernandes0763@gmail.com>
2026-07-27 10:30:21 -0300

    criando dimensao areas_comercializacao para servir de bridge table na fato

---
+++ dbt_dw_ans/models/dw/dimensions/dim_areas_comercializacao.sql
@@ -0,0 +1,13 @@
+SELECT
+    areas.id_plano,
+    areas.cd_operadora,
+    areas.dt_ntrp,
+    areas.cd_municipio
+FROM ref({{ 'tr_areas_comercializacao_planos' }}) areas
+WHERE cd_nota = (
+    SELECT MAX(cd_nota)
+    FROM ref({{ 'tr_areas_comercializacao_planos' }}) areas2
+    WHERE areas.id_plano = areas2.id_plano
+    AND areas.cd_nota = areas2.cd_nota,
+    AND areas.dt_ntrp = areas2.dt_ntrp
+)
