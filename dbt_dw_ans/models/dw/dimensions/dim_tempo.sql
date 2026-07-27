b77932ddba8fa8ba3a52d06c2c119b402ce04507
vitfernands <vitorfernandes0763@gmail.com>
2026-07-27 10:37:57 -0300

    alterando dim_tempo

--- dbt_dw_ans/models/dw/dimensions/dim_tempo.sql
+++ dbt_dw_ans/models/dw/dimensions/dim_tempo.sql
@@ -1,18 +1,13 @@
-{{ config(materialized='table') }}
-
-WITH dates AS (
-    {{ dbt_utils.date_spine(
-        datepart   = "day",
-        start_date = "cast('2020-01-01' as date)",
-        end_date   = "cast('2030-12-31' as date)"
-    ) }}
-)
-
 SELECT
-    {{ dbt_utils.generate_surrogate_key(['date_day']) }} AS id_tempo,
-    date_day                                             AS data,
-    EXTRACT(YEAR    FROM date_day)::INT                  AS ano,
-    EXTRACT(MONTH   FROM date_day)::INT                  AS nr_mes,
-    EXTRACT(QUARTER FROM date_day)::INT                  AS trimestre,
-    TO_CHAR(date_day, 'TMMonth')                         AS nm_mes
+    CAST(TO_CHAR(date_day, 'YYYYMMDD') AS INT)   AS id_tempo,
+    date_day                                      AS data,
+    EXTRACT(YEAR    FROM date_day)::INT           AS ano,
+    EXTRACT(QUARTER FROM date_day)::INT           AS trimestre,
+    EXTRACT(MONTH   FROM date_day)::INT           AS nr_mes,
+    CASE EXTRACT(MONTH FROM date_day) ... END     AS nm_mes,
+    LEFT(CASE EXTRACT(MONTH FROM date_day) ... END, 3) AS nm_mes_abrev,
+    EXTRACT(YEAR FROM date_day)::INT * 100 + EXTRACT(MONTH FROM date_day)::INT AS ano_mes,   
+    EXTRACT(DAY     FROM date_day)::INT           AS dia,
+    CASE WHEN EXTRACT(MONTH FROM date_day) <= 6 THEN 1 ELSE 2 END AS semestre,
+    (date_day = (DATE_TRUNC('month', date_day) + INTERVAL '1 month' - INTERVAL '1 day')) AS fl_ultimo_dia_mes
 FROM dates
