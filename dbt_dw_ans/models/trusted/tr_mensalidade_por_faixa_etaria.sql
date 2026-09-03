3dc113fdfe9c7213311a6a4c0457be00b570a2af
vitfernands <vitorfernandes0763@gmail.com>
2026-09-03 00:00:34 -0300

    refatorando codigos, apagando tabelas desnecessárias, filtrando apenas por sc e removendo cd_nota desnecessarios

--- dbt_dw_ans/models/trusted/tr_mensalidade_por_faixa_etaria.sql
+++ dbt_dw_ans/models/trusted/tr_mensalidade_por_faixa_etaria.sql
@@ -8,16 +8,48 @@
     ]
 ) }}
 
+WITH dados AS (
+    SELECT
+        TRIM(mfe.cd_operadora)::INT                        AS cd_operadora,
+        TRIM(mfe.id_plano)::INT                            AS id_plano,
+        TRIM(mfe.cd_nota)::INT                             AS cd_nota,
+        TRIM(mfe.dt_ntrp)::DATE                            AS dt_ntrp,
+        TRIM(mfe.id_abrg)::VARCHAR(100)                    AS id_abrg,
+        TRIM(mfe.faixa_etaria)::VARCHAR(100)               AS faixa_etaria,
+        TRIM(mfe.vl_comercial_mensalidade)::DECIMAL(10, 2) AS vl_comercial_mensalidade,
+        TRIM(mfe.vl_desp_assistencial)::DECIMAL(10, 2)     AS vl_desp_assistencial,
+        TRIM(mfe.vcm_minimo)::DECIMAL(10, 2)               AS vcm_minimo,
+        TRIM(mfe.vcm_maximo)::DECIMAL(10, 2)               AS vcm_maximo,
+        TRIM(mfe.dt_atualizacao)::DATE                     AS dt_atualizacao,
+
+        ROW_NUMBER() OVER (
+            PARTITION BY 
+                mfe.id_plano,
+                mfe.dt_ntrp,
+                mfe.faixa_etaria
+            ORDER BY
+                mfe.cd_nota DESC
+        ) AS rn
+
+    FROM {{ source('stage', 'mensalidade_por_faixa_etaria') }} mfe
+    WHERE EXISTS (
+        SELECT 1 
+        FROM {{ ref('tr_areas_comercializacao_planos') }} acp 
+        WHERE acp.id_plano = mfe.id_plano
+    )
+)
+
 SELECT
-    TRIM(cd_operadora)::INT                        AS cd_operadora,
-    TRIM(id_plano)::INT                            AS id_plano,
-    TRIM(cd_nota)::INT                             AS cd_nota,
-    TO_DATE(dt_ntrp, 'DD/MM/YYYY')                 AS dt_ntrp,
-    TRIM(id_abrg)::VARCHAR(100)                    AS id_abrg,
-    TRIM(faixa_etaria)::VARCHAR(100)               AS faixa_etaria,
-    TRIM(vl_comercial_mensalidade)::DECIMAL(10, 2) AS vl_comercial_mensalidade,
-    TRIM(vl_desp_assistencial)::DECIMAL(10, 2)     AS vl_desp_assistencial,
-    TRIM(vcm_minimo)::DECIMAL(10, 2)               AS vcm_minimo,
-    TRIM(vcm_maximo)::DECIMAL(10, 2)               AS vcm_maximo,
-    TO_DATE(dt_atualizacao, 'DD/MM/YYYY')          AS dt_atualizacao
-FROM {{source('stage', 'mensalidade_por_faixa_etaria')}}
+    cd_operadora,
+    id_plano,
+    cd_nota,
+    dt_ntrp,
+    id_abrg,
+    faixa_etaria,
+    vl_comercial_mensalidade,
+    vl_desp_assistencial,
+    vcm_minimo,
+    vcm_maximo,
+    dt_atualizacao
+FROM dados
+WHERE rn = 1
