505cfdab5fc17dde926de540b138cca2dfcf55e6
vitfernands <vitorfernandes0763@gmail.com>
2026-09-07 14:40:20 -0300

    correcao de nome da tabela

--- dbt_dw_ans/models/dw/facts/fato_areas_comercializacao_planos.sql
+++ dbt_dw_ans/models/dw/facts/fato_areas_comercializacao_planos.sql
@@ -7,4 +7,4 @@ SELECT
 FROM {{ ref('tr_areas_comercializacao_planos') }} acp
 JOIN {{ ref('dim_plano') }} p ON acp.id_plano = p.id_plano
 JOIN {{ ref('dim_operadoras') }} o ON o.cd_operadora = acp.cd_operadora
-JOIN {{ ref('dim_municipios ') }} m ON m.cd_municipio = acp.cd_municipio
+JOIN {{ ref('dim_municipios') }} m ON m.cd_municipio = acp.cd_municipio
